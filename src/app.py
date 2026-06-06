339a2c4ab6343d8f0b58267221b705cba09a7a5f
Edgard Oporto <edgard.oporto@gmail.com>
2026-06-06 15:50:32 -0500

    Font

--- src/app.py
+++ src/app.py
@@ -6,6 +6,27 @@ from modules.dashboard import graficar_ecg_crudo
 
 st.set_page_config(page_title="AFibDetect System", page_icon="🩺", layout="wide")
 
+if "df_ecg" not in st.session_state:
+    st.session_state["df_ecg"] = None
+
+st.title("🩺 AFibDetect v1.0")
+
+archivo_subido = st.file_uploader("Cargar registro ECG (Formato CSV)", type=["csv"])
+
+if archivo_subido is not None:
+    df_temporal = cargar_archivo_csv(archivo_subido)
+    es_valido, mensaje = validar_estructura_ecg(df_temporal)
+    
+    if es_valido:
+        st.session_state["df_ecg"] = df_temporal
+        st.success(mensaje)
+        graficar_ecg_crudo(st.session_state["df_ecg"])
+    else:
+        st.error(mensaje)
+
+
+
+
 # Inyección de código CSS para reducir el espacio vertical superior
 st.markdown(
     """
@@ -36,23 +57,3 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-
-if "df_ecg" not in st.session_state:
-    st.session_state["df_ecg"] = None
-
-st.title("🩺 AFibDetect v1.0")
-
-archivo_subido = st.file_uploader("Cargar registro ECG (Formato CSV)", type=["csv"])
-
-if archivo_subido is not None:
-    df_temporal = cargar_archivo_csv(archivo_subido)
-    es_valido, mensaje = validar_estructura_ecg(df_temporal)
-    
-    if es_valido:
-        st.session_state["df_ecg"] = df_temporal
-        st.success(mensaje)
-        graficar_ecg_crudo(st.session_state["df_ecg"])
-    else:
-        st.error(mensaje)
-
-

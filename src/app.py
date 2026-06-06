1912a779bcb4b0e46266bbe26d52c582c4239bfc
Edgard Oporto <edgard.oporto@gmail.com>
2026-06-06 15:48:14 -0500

    Font

--- src/app.py
+++ src/app.py
@@ -24,7 +24,7 @@ st.markdown(
 
         /* Controla el tamaño de los subtítulos de secciones (st.header) */
         h2 {
-            font-size: 1.2rem !important;
+            font-size: 0.9rem !important;
             padding-top: 0.5rem;
         }
         
